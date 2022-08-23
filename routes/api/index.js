bc276ee27f9c274a4a8144cd936db446928e4084
Josue Hernandez <joshuamo574@gmail.com>
2022-08-23 16:13:56 -0700

    Fixed Bugs with Tests

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,5 +7,7 @@ router.use('/auth', require('./auth'));
 router.use('/passwords', require('./passwords'));
 router.use('/users', require('./users'));
 router.use('/housings', require('./housings'));
+router.use('/legals', require('./legals'));
+router.use('/foods', require('./foods'));
 
 module.exports = router;
--- test/routes/api/foods.js
+++ test/routes/api/foods.js
@@ -19,7 +19,7 @@ describe('/api/foods', () => {
     it('returns a list of Foods', async () => {
       const response = await testSession.get('/api/foods').expect(HttpStatus.OK);
       const cat = response.body;
-      assert.deepStrictEqual(res.length, 2);
+      assert.deepStrictEqual(cat.length, 2);
     });
   });
   describe('GET /:id', () => {
@@ -52,7 +52,6 @@ describe('/api/foods', () => {
           .post('/api/foods')
           .set('Accept', 'application/json')
           .send({
-            id: 1,
             Name: 'This is a new Food Name.',
             Description: 'This is a new Food Description.',
             PhoneNumber: 'This is a new Food PhoneNumber.',
@@ -61,23 +60,21 @@ describe('/api/foods', () => {
           })
           .expect(HttpStatus.CREATED);
 
-        const { id, Name, IconBackImg, NavBackImg, Position } = response.body;
+        const { id, Name, Description, PhoneNumber, Address, Email } = response.body;
         assert(id);
-        assert.deepStrictEqual(id, 1);
-        assert.deepStrictEqual(Name, 'This is a new Food Name');
-        assert.deepStrictEqual(Description, 'This is a new Food Description');
-        assert.deepStrictEqual(PhoneNumber, 'This is a new Food Phone Number');
-        assert.deepStrictEqual(Address, 'This is a new Food Address');
-        assert.deepStrictEqual(Email, 'This is a new Food Email');
+        assert.deepStrictEqual(Name, 'This is a new Food Name.');
+        assert.deepStrictEqual(Description, 'This is a new Food Description.');
+        assert.deepStrictEqual(PhoneNumber, 'This is a new Food PhoneNumber.');
+        assert.deepStrictEqual(Address, 'This is a new Food Address.');
+        assert.deepStrictEqual(Email, 'This is a new Food Email.');
 
-        const cat = await models.Category.findByPk(id);
+        const cat = await models.Food.findByPk(id);
         assert(cat);
-        assert.deepStrictEqual(cat.id, 1);
-        assert.deepStrictEqual(Name, 'This is a new Food Name');
-        assert.deepStrictEqual(Description, 'This is a new Food Description');
-        assert.deepStrictEqual(PhoneNumber, 'This is a new Food Phone Number');
-        assert.deepStrictEqual(Address, 'This is a new Food Address');
-        assert.deepStrictEqual(Email, 'This is a new Food Email');
+        assert.deepStrictEqual(Name, 'This is a new Food Name.');
+        assert.deepStrictEqual(Description, 'This is a new Food Description.');
+        assert.deepStrictEqual(PhoneNumber, 'This is a new Food PhoneNumber.');
+        assert.deepStrictEqual(Address, 'This is a new Food Address.');
+        assert.deepStrictEqual(Email, 'This is a new Food Email.');
       });
     });
 
@@ -87,31 +84,30 @@ describe('/api/foods', () => {
           .patch('/api/foods/1')
           .set('Accept', 'application/json')
           .send({
-            id: 1,
             Name: 'This is an updated Food Name.',
-            Description: 'This is an updated Food Description',
+            Description: 'This is an updated Food Description.',
             PhoneNumber: 'This is an updated Food PhoneNumber.',
             Address: 'This is an updated Food Address.',
             Email: 'This is an updated Food Email.',
           })
           .expect(HttpStatus.OK);
 
-        const { id, Name, IconBackImg, NavBackImg, Position } = response.body;
+        const { id, Name, Description, PhoneNumber, Address, Email } = response.body;
         assert.deepStrictEqual(id, 1);
-        assert.deepStrictEqual(Name, 'This is an updated Food Name');
-        assert.deepStrictEqual(Description, 'This is an updated Food Description');
-        assert.deepStrictEqual(PhoneNumber, 'This is an updated Food PhoneNumber');
-        assert.deepStrictEqual(Address, 'This is an updated Food Address');
-        assert.deepStrictEqual(Email, 'This is an updated Food Email');
+        assert.deepStrictEqual(Name, 'This is an updated Food Name.');
+        assert.deepStrictEqual(Description, 'This is an updated Food Description.');
+        assert.deepStrictEqual(PhoneNumber, 'This is an updated Food PhoneNumber.');
+        assert.deepStrictEqual(Address, 'This is an updated Food Address.');
+        assert.deepStrictEqual(Email, 'This is an updated Food Email.');
 
         const cat = await models.Food.findByPk(id);
         assert(cat);
         assert.deepStrictEqual(cat.id, 1);
-        assert.deepStrictEqual(Name, 'This is an updated Food Name');
-        assert.deepStrictEqual(Description, 'This is an updated Food Description');
-        assert.deepStrictEqual(PhoneNumber, 'This is an updated Food PhoneNumber');
-        assert.deepStrictEqual(Address, 'This is an updated Food Address');
-        assert.deepStrictEqual(Email, 'This is an updated Food Email');
+        assert.deepStrictEqual(Name, 'This is an updated Food Name.');
+        assert.deepStrictEqual(Description, 'This is an updated Food Description.');
+        assert.deepStrictEqual(PhoneNumber, 'This is an updated Food PhoneNumber.');
+        assert.deepStrictEqual(Address, 'This is an updated Food Address.');
+        assert.deepStrictEqual(Email, 'This is an updated Food Email.');
       });
     });
 
